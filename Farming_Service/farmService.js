5a2e06a0cf46dcfa9265cb45c3dc9d5f322c62be
arpit.khandelwal.1984 <arpit.khandelwal.1984@gmail.com>
2018-08-24 18:06:55 +0530

    Initial Commit

---
+++ Farming_Service/farmService.js
@@ -0,0 +1,112 @@
+'use strict';
+const path = require('path');
+const fse = require('fs-extra');
+const redis = require('redis');
+
+var redisClient; //Initialize when necessary
+
+var farm = []; // global in-memory farm to host the directories
+var refereshInProgress = false;
+/*
+ * InitFarm method
+ *
+ */
+function initFarm(options) {
+    try {
+        // Log to the console 
+        console.log("========== Farming Started ==========");
+        console.log("Caching mechanism : " + options.cache);
+        console.log("Source : " + options.source); 
+        console.log("Destination Farm : " + options.destination);
+        console.log("Farm will refresh every : " + options.interval + " seconds");
+
+        if("redis" == options.cache){
+            console.log("Redis Host : " + options.redisHost );
+            console.log("Redis Port : " + options.redisPort );
+            redisClient = redis.createClient(options.redisPort, options.redisHost);
+        }
+        console.log("=================================");
+        // Schedule the copy at given interval
+        let interval = options.interval * 1000; // convert seconds to milliseconds
+        setInterval(()=>{refreshFarm(options);}, interval);        
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+/*
+ * Method to return next available entitiy in the farm
+ */
+function getNext() {
+    return new Promise(function(resolve, reject) {
+        if(redisClient){ // cache = Redis
+            redisClient.rpop("farm", function(err, result) {
+                if(err || !result) {
+                    reject("Farm is empty at the moment.");
+                }
+                resolve(result);
+            });
+        } else{
+            if(farm.length > 0) { // cache = In Memory
+                resolve(farm.pop());
+            } else {
+                reject("Farm is empty at the moment.");
+            }
+        }
+    });
+}
+
+/*
+ * Refresh the Farm at given interval and fill up the deficit of directories
+ */
+async function refreshFarm(options) {
+    if(!refereshInProgress){
+        let source = options.source;
+        let destination = options.destination;
+        let activeClonesNeeded = options.activeCloneCount;
+
+        let currentActiveClones = await getFarmStatus();
+        let difference = activeClonesNeeded - currentActiveClones;
+        console.log("currentActiveClones = " + currentActiveClones + " difference = " + difference);
+        if(difference > 0) { // Need to clone
+            while (difference > 0) {
+                let dir = Date.now().toString();
+                let dirPath = path.join(destination, dir);
+                fse.copySync(source, dirPath);
+                if(redisClient) { // cache = Redis
+                    redisClient.rpush("farm", dir);
+                } else { // cache = In-Memory
+                    farm.push(dir);
+                }
+                difference--; // Reduce the deficit
+            }
+            console.log("Finished refresh, cloned " + (activeClonesNeeded - currentActiveClones) + " entities");
+        } else{
+            console.log("Sufficient entities are available, farming not needed.");
+        }
+    } else {
+        console.log("A refresh opertion is in progress, will check again in " + options.interval + " seconds");
+    }
+}
+
+/* 
+ Returns the active clone count from the Redis or In-Memory Farm
+*/
+async function getFarmStatus() {
+    return new Promise(function(resolve, reject) {
+        if(redisClient){ // cache = Redis
+            redisClient.llen("farm", function(err, result) {
+                let count = result;
+                if(err || !result) {
+                    count = 0;
+                }
+                resolve(count);
+            });
+        } else{
+            resolve(farm.length); // cache = In Memory
+        }
+    });
+}
+
+module.exports.initFarm = initFarm;
+module.exports.getNext = getNext;
